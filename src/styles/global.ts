63df761cf5b37f1db7004c9105f8c68d0f6763fb
Muratov Ivan <ivanmuratov121096@gmail.com>
2022-10-03 17:21:29 +0700

    deployed to vercel

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,13 +3,13 @@ import { createGlobalStyle } from 'styled-components/macro';
 export const GlobalStyles = createGlobalStyle`
   html,
   body {
-    height: 100%;
+    min-height: 100vh;
   }
 
   body {
     font-family: 'Urbanist', sans-serif;
 	  font-weight: 600;
-    background: ${(props) => props.theme.background} fixed;
+    background: ${(props) => props.theme.background} no-repeat 0 0 fixed;
     color: ${(props) => props.theme.color};  
     transition: background 0.5s linear, color 0.5s linear;
   }  
